e0e8e8dcf5add1b10a9e027a18ba86daa40e8b82
GordonGichini <gordongichini1@gmail.com>
2022-06-15 23:46:03 +0300

    commit tree 4d9ab51ae77532244b286d170620324510ad62f5

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // code your solution here
+function findMatching(source, sought){
+    return source.filter(
+        (possibleMatch) => possibleMatch.toLowerCase() ===sought.toLowerCase
+
+    );
+    }
+    function fuzzyMatch(source, testString){
+        return source.filter(
+            (possibleMatch) =>
+            posssibleMatch.toLowerCase().indexof(testString.toLowerCase()) === 0
+        );
+    }
+    function matchName(source, soughtName) {
+        return source.filter((record) => record.name ===soughtName);
+    }
